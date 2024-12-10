fb319e1d0314250e0399120a01ff07bf2ae39962
claustra01 <shinya430.g@gmail.com>
2024-12-11 02:25:04 +0900

    多対多マッチング

--- go/internal_handlers.go
+++ go/internal_handlers.go
@@ -9,9 +9,10 @@ import (
 // このAPIをインスタンス内から一定間隔で叩かせることで、椅子とライドをマッチングさせる
 func internalGetMatching(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
-	// MEMO: 一旦最も待たせているリクエストに適当な空いている椅子マッチさせる実装とする。おそらくもっといい方法があるはず…
-	ride := &Ride{}
-	if err := db.GetContext(ctx, ride, `SELECT * FROM rides WHERE chair_id IS NULL ORDER BY created_at LIMIT 1`); err != nil {
+
+	// MEMO: 一旦多対多でマッチングするようにしてみる。距離は考慮していないため後で改善後で改善する。
+	rideList := []Ride{}
+	if err := db.SelectContext(ctx, &rideList, `SELECT * FROM rides WHERE chair_id IS NULL ORDER BY created_at`); err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
 			w.WriteHeader(http.StatusNoContent)
 			return
@@ -20,33 +21,37 @@ func internalGetMatching(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	matched := &Chair{}
-	empty := false
-	for i := 0; i < 10; i++ {
-		if err := db.GetContext(ctx, matched, "SELECT * FROM chairs INNER JOIN (SELECT id FROM chairs WHERE is_active = TRUE ORDER BY RAND() LIMIT 1) AS tmp ON chairs.id = tmp.id LIMIT 1"); err != nil {
-			if errors.Is(err, sql.ErrNoRows) {
-				w.WriteHeader(http.StatusNoContent)
-				return
-			}
-			writeError(w, http.StatusInternalServerError, err)
+	chairList := []Chair{}
+	if err := db.SelectContext(ctx, &chairList, "SELECT * FROM chairs WHERE is_active = TRUE ORDER BY RAND()"); err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			w.WriteHeader(http.StatusNoContent)
+			return
 		}
+		writeError(w, http.StatusInternalServerError, err)
+		return
+	}
 
+	rideListIndex := 0
+	for j := 0; j < len(chairList); j++ {
+		matched := chairList[j]
+		empty := false
 		if err := db.GetContext(ctx, &empty, "SELECT COUNT(*) = 0 FROM (SELECT COUNT(chair_sent_at) = 6 AS completed FROM ride_statuses WHERE ride_id IN (SELECT id FROM rides WHERE chair_id = ?) GROUP BY ride_id) is_completed WHERE completed = FALSE", matched.ID); err != nil {
 			writeError(w, http.StatusInternalServerError, err)
 			return
 		}
-		if empty {
+		if !empty {
+			continue
+		}
+
+		ride := rideList[rideListIndex]
+		if _, err := db.ExecContext(ctx, "UPDATE rides SET chair_id = ? WHERE id = ?", matched.ID, ride.ID); err != nil {
+			writeError(w, http.StatusInternalServerError, err)
+			return
+		}
+		rideListIndex++
+		if rideListIndex >= len(rideList) {
 			break
 		}
-	}
-	if !empty {
-		w.WriteHeader(http.StatusNoContent)
-		return
-	}
-
-	if _, err := db.ExecContext(ctx, "UPDATE rides SET chair_id = ? WHERE id = ?", matched.ID, ride.ID); err != nil {
-		writeError(w, http.StatusInternalServerError, err)
-		return
 	}
 
 	w.WriteHeader(http.StatusNoContent)
